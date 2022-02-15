0b4baa067a5fe3c9d86f1313756c7fe3e87b3178
Hex <zhaochang@changba.com>
2022-02-15 17:00:53 +0800

    修复日期选择器的一个 BUG

--- test/box/box.spec.js
+++ test/box/box.spec.js
@@ -1,9 +1,36 @@
 import { shallowMount } from '@vue/test-utils';
 import Component from '../../packages/box/src/box.vue';
 
+const factory = (propsData) => {
+  return shallowMount(Component, {
+    propsData: {
+      ...propsData
+    }
+  })
+}
+
 describe('box', () => {
   it('renders dom when mounted', () => {
-    const wrapper = shallowMount(Component);
+    const wrapper = factory();
     expect(wrapper.classes()).toContain('cb-box');
+  });
+
+  it('renders props.theme when passed', () => {
+    const theme = 'primary';
+    const wrapper = factory({ theme });
+
+    expect(wrapper.classes()).toContain(`box-${theme}`);
+  });
+
+  it('renders props.collapsed when passed', () => {
+    const wrapper = factory({ collapsed: true });
+
+    expect(wrapper.classes()).toContain('collapsed-box');
+  });
+
+  it('renders props.solid when passed', () => {
+    const wrapper = factory({ solid: true });
+
+    expect(wrapper.classes()).toContain('box-solid');
   });
 });
